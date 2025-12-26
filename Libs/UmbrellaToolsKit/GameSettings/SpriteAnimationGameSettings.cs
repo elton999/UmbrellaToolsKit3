331ce76be1c2ddbc0553dbf25afef4ae2f628fab
Elton Silva <eltoncarsilva73@gmail.com>
2025-12-26 15:08:20 -0300

    created sprite animation game settings

---
+++ Libs/UmbrellaToolsKit/GameSettings/SpriteAnimationGameSettings.cs
@@ -0,0 +1,10 @@
+using UmbrellaToolsKit.EditorEngine.Attributes;
+
+namespace UmbrellaToolsKit.EditorEngine.GameSettings
+{
+    [GameSettingsProperty(nameof(SpriteAnimationGameSettings), "/Content/")]
+    public class SpriteAnimationGameSettings : GameSettingsProperty
+    {
+
+    }
+}
