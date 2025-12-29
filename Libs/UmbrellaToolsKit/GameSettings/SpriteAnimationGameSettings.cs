6b16c5fc5b3708e717be3b1426d03dcfe39f523e
Elton Silva <eltoncarsilva73@gmail.com>
2025-12-29 16:06:53 -0300

    logica to show time line cursor

--- Libs/UmbrellaToolsKit/GameSettings/SpriteAnimationGameSettings.cs
+++ Libs/UmbrellaToolsKit/GameSettings/SpriteAnimationGameSettings.cs
@@ -3,7 +3,7 @@ using ImGuiNET;
 using System.Collections.Generic;
 using System.Numerics;
 using UmbrellaToolsKit.EditorEngine.Primitives;
-
+using System;
 
 namespace UmbrellaToolsKit.EditorEngine.GameSettings
 {
@@ -18,8 +18,8 @@ namespace UmbrellaToolsKit.EditorEngine.GameSettings
             var timeLinePosition = new Vector2(position.X + timeLineSettings.GetPositionXOnTimeLine(Start), position.Y);
             Square.Draw(
                 drawList,
-                new Microsoft.Xna.Framework.Vector2(timeLinePosition.X, timeLinePosition.Y),
-                new Microsoft.Xna.Framework.Vector2(timeLineSettings.GetPositionXOnTimeLine(Duration), timeLineSettings.TimelineHight),
+                new Microsoft.Xna.Framework.Vector2(timeLinePosition.X, timeLinePosition.Y + 1f),
+                new Microsoft.Xna.Framework.Vector2(timeLineSettings.GetPositionXOnTimeLine(Duration), timeLineSettings.TimeLineHight - 2f),
                 Microsoft.Xna.Framework.Color.Red
             );
 
@@ -45,13 +45,21 @@ namespace UmbrellaToolsKit.EditorEngine.GameSettings
     public class SpriteAnimationGameSettings : GameSettingsProperty
     {
         private TimelineItem _selected;
+        private float _currentTime = 0.5f;
         private float _durationInSeconds = 2f;
         private float _totalFramesInWindow = 250f;
         private float _framePerSecond = 60f;
         private int _frameStep = 15;
-        private float _timelineHight = 15f;
+        private float _timelineRuleHight = 15f;
         private float _stepSize;
+        private float _timeLineHight = 30f;
         private Vector2 _timeLinePosition;
+
+        private List<Type> _timeLineItemTypes = new List<Type>()
+        {
+            typeof(TimelineEvent),
+            typeof(TimelineSequence),
+        };
 
         public List<List<TimelineItem>> TimeLines = new List<List<TimelineItem>>()
         {
@@ -71,21 +79,37 @@ namespace UmbrellaToolsKit.EditorEngine.GameSettings
         public float TotalFramesInWindow { get => _totalFramesInWindow; set => _totalFramesInWindow = value; }
         public float FramePerSecond { get => _framePerSecond; set => _framePerSecond = value; }
         public int FrameStep { get => _frameStep; set => _frameStep = value; }
-        public float TimelineHight { get => _timelineHight; set => _timelineHight = value; }
+        public float TimelineRuleHight { get => _timelineRuleHight; set => _timelineRuleHight = value; }
         public Vector2 TimeLinePosition { get => _timeLinePosition; set => _timeLinePosition = value; }
         public float StepSize { get => _stepSize; set => _stepSize = value; }
+        public float TimeLineHight { get => _timeLineHight; set => _timeLineHight = value; }
 
-        public void Draw(uint dockId)
+        public void DrawTimeLine(uint dockId)
         {
             ImGui.SetNextWindowDockID(dockId, ImGuiCond.Once);
-            ImGui.Begin("Timeline", ImGuiWindowFlags.NoScrollbar);
+            ImGui.Begin("TimelineDock", ImGuiWindowFlags.NoScrollbar);
 
             _timeLinePosition = ImGui.GetCursorScreenPos();
             var drawList = ImGui.GetWindowDrawList();
 
             DrawTimeLineRule(drawList, _timeLinePosition);
             DrawTimeLines(drawList, _timeLinePosition);
+            DrawTimeCursor(drawList, _timeLinePosition);
 
+            ImGui.End();
+        }
+
+        public void DrawProperties(uint dockId)
+        {
+            ImGui.SetNextWindowDockID(dockId, ImGuiCond.Once);
+            ImGui.Begin("PropertiesDock");
+            foreach (var timeLineItem in _timeLineItemTypes)
+            {
+                if (Fields.Buttons.BlueButton($"Add {AttributesHelper.FormatName(timeLineItem.Name)}"))
+                {
+
+                }
+            }
             ImGui.End();
         }
 
@@ -94,20 +118,20 @@ namespace UmbrellaToolsKit.EditorEngine.GameSettings
             float timeLineWidth = ImGui.GetWindowSize().X;
 
             int timeLineCount = 0;
-            float offsetY = _timelineHight;
+            float offsetY = _timelineRuleHight;
             foreach (var timelineItem in TimeLines)
             {
-                float yPosition = position.Y + offsetY * timeLineCount;
+                float yPosition = position.Y + offsetY + TimeLineHight * timeLineCount;
                 Square.Draw(
                     drawList,
-                    new Microsoft.Xna.Framework.Vector2(position.X, yPosition + offsetY),
-                    new Microsoft.Xna.Framework.Vector2(timeLineWidth, _timelineHight),
+                    new Microsoft.Xna.Framework.Vector2(position.X, yPosition),
+                    new Microsoft.Xna.Framework.Vector2(timeLineWidth, TimeLineHight),
                     Microsoft.Xna.Framework.Color.White
                 );
                 Square.Draw(
                     drawList,
-                    new Microsoft.Xna.Framework.Vector2(position.X, yPosition + offsetY + 1),
-                    new Microsoft.Xna.Framework.Vector2(timeLineWidth, _timelineHight - 2),
+                    new Microsoft.Xna.Framework.Vector2(position.X, yPosition + 1),
+                    new Microsoft.Xna.Framework.Vector2(timeLineWidth, TimeLineHight - 2),
                     Microsoft.Xna.Framework.Color.Black
                 );
 
@@ -117,7 +141,7 @@ namespace UmbrellaToolsKit.EditorEngine.GameSettings
             timeLineCount = 0;
             foreach (var timeLine in TimeLines)
             {
-                float yPosition = position.Y + offsetY * timeLineCount + offsetY;
+                float yPosition = position.Y + offsetY + TimeLineHight * timeLineCount;
                 foreach (var timeLineItem in timeLine)
                 {
                     timeLineItem.Draw(drawList, new Vector2(position.X, yPosition), this);
@@ -134,8 +158,8 @@ namespace UmbrellaToolsKit.EditorEngine.GameSettings
             int totalFrames = (int)(_durationInSeconds * _framePerSecond);
 
             drawList.AddLine(
-                new Vector2(position.X, position.Y + _timelineHight),
-                new Vector2(position.X + timeLineWidth, position.Y + _timelineHight),
+                new Vector2(position.X, position.Y + _timelineRuleHight),
+                new Vector2(position.X + timeLineWidth, position.Y + _timelineRuleHight),
                 ImGui.GetColorU32(Vector4.One)
             );
 
@@ -150,15 +174,43 @@ namespace UmbrellaToolsKit.EditorEngine.GameSettings
             }
         }
 
+        public void DrawTimeCursor(ImDrawListPtr drawList, Vector2 position)
+        {
+            float xPosition = GetPositionXOnTimeLine(_currentTime);
+
+            drawList.AddTriangleFilled
+            (
+                new Vector2(xPosition + position.X - 5f, position.Y),
+                new Vector2(xPosition + position.X + 5f, position.Y),
+                new Vector2(xPosition + position.X, position.Y + 5f),
+                ImGui.GetColorU32(Microsoft.Xna.Framework.Color.Yellow.PackedValue)
+            );
+
+            drawList.AddLine
+            (
+               new Vector2(xPosition + position.X, position.Y),
+               new Vector2(xPosition + position.X, position.Y + 200f),
+               ImGui.GetColorU32(Microsoft.Xna.Framework.Color.Yellow.PackedValue)
+            );
+
+        }
+
         public override void DrawFields(EditorMain editorMain)
         {
+            uint idProperties = ImGui.GetID("Properties");
             uint idTimeline = ImGui.GetID("Timeline");
 
-            ImGui.BeginChild("timelineMain", new Vector2(ImGui.GetWindowWidth()));
+            ImGui.BeginChild("properties", new Vector2(ImGui.GetWindowWidth() * 0.2f));
+            ImGui.DockSpace(idProperties, new Vector2(0, 0));
+            ImGui.EndChild();
+            ImGui.SameLine();
+
+            ImGui.BeginChild("timelineMain", new Vector2(ImGui.GetWindowWidth() * 0.8f));
             ImGui.DockSpace(idTimeline, new Vector2(0, 0));
             ImGui.EndChild();
 
-            Draw(idTimeline);
+            DrawTimeLine(idTimeline);
+            DrawProperties(idProperties);
         }
 
         public float GetPositionXOnTimeLine(float valueInSeconds)
