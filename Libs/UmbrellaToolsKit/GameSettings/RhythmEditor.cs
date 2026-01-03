39d649465a81497556ea199ebfacaa69ce6acc8e
Elton Silva <eltoncarsilva73@gmail.com>
2026-01-03 03:01:48 -0300

    logic to load a song

--- Libs/UmbrellaToolsKit/GameSettings/RhythmEditor.cs
+++ Libs/UmbrellaToolsKit/GameSettings/RhythmEditor.cs
@@ -12,6 +12,7 @@ namespace UmbrellaToolsKit.EditorEngine.GameSettings
     public class EventItem : TimelineItem
     {
         protected override Framework.Color _color => Framework.Color.Green;
+        protected override bool _showName => false;
 
         public enum Keys
         {
@@ -32,20 +33,87 @@ namespace UmbrellaToolsKit.EditorEngine.GameSettings
     public class MusicItem : TimelineItem
     {
         public string MusicPath;
-        public float StartMusicAt;
+        [ShowEditor] public float StartMusicAt;
         private Framework.Media.Song _song;
+        private bool _isPlaying = false;
+
+        public MusicItem()
+        {
+            if (string.IsNullOrEmpty(MusicPath))
+                return;
+            SetMusicFromPath(MusicPath);
+        }
+
+        private void SetMusicFromPath(string path)
+        {
+            if (string.IsNullOrEmpty(path) || !System.IO.File.Exists(path))
+                return;
+
+            var fullPath = System.IO.Path.GetFullPath(path);
+            var uri = new Uri(fullPath, UriKind.Absolute);
+
+            try
+            {
+                _song = Framework.Media.Song.FromUri(System.IO.Path.GetFileName(fullPath), uri);
+                Duration = (float)(_song?.Duration.TotalSeconds ?? 0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to load song '{path}': {ex.Message}");
+                _song = null;
+                MusicPath = string.Empty;
+            }
+        }
 
         public override void DrawProperties()
         {
             InspectorClass.DrawAllFields(this);
+            if (string.IsNullOrEmpty(MusicPath))
+            {
+                ImGui.Text("No music loaded");
+            }
+
+            if (!string.IsNullOrEmpty(MusicPath))
+            {
+                ImGui.Text($"Loaded music: {System.IO.Path.GetFileName(MusicPath)}");
+            }
+
+            if (Fields.Buttons.BlueButton("Load Music"))
+            {
+                var openFileDialog = OpenFileDialogue.OpenFileDialog("Import music", "Music", ".ogg");
+                if (OpenFileDialogue.SaveFileDialog(openFileDialog))
+                {
+                    string filePath = openFileDialog.FileName;
+                    if (!string.IsNullOrEmpty(filePath))
+                    {
+                        MusicPath = filePath;
+                        SetMusicFromPath(MusicPath);
+                    }
+                }
+            }
         }
 
         public override void Draw(ImDrawListPtr drawList, Vector2 position, TimeLineFeature timeLineSettings)
         {
             base.Draw(drawList, position, timeLineSettings);
-        }
 
-        private void SetSong(Framework.Media.Song song) => _song = song;
+            if (timeLineSettings.CurrentState is TimeLineFeature.State.PLAYING && _song != null && !_isPlaying)
+            {
+                if (timeLineSettings.CurrentTimeInSeconds >= Start && timeLineSettings.CurrentTimeInSeconds <= Start + Duration)
+                {
+                    float songOffset = (timeLineSettings.CurrentTimeInSeconds - Start) * 1.0f;
+                    var songPosition = TimeSpan.FromSeconds(StartMusicAt + songOffset);
+                    Framework.Media.MediaPlayer.Play(_song, songPosition);
+                    _isPlaying = true;
+                }
+            }
+
+            if (timeLineSettings.CurrentState is TimeLineFeature.State.STOPPED && _song != null)
+            {
+                Framework.Media.MediaPlayer.Stop();
+                _isPlaying = false;
+            }
+        }
     }
 
     public class RhythmTimeLine : TimeLineFeature
@@ -53,13 +121,8 @@ namespace UmbrellaToolsKit.EditorEngine.GameSettings
         protected override List<Type> _timeLineItemTypes => new List<Type>()
         {
             typeof(MusicItem),
-            typeof(RhythmTimeLine),
+            typeof(EventItem),
         };
-
-        public void CallOnUpdateTimeLineData(Action callback)
-        {
-            callback?.Invoke();
-        }
 
         public override void AddANewItem(Type timeLineItem, object item)
         {
@@ -72,11 +135,6 @@ namespace UmbrellaToolsKit.EditorEngine.GameSettings
     {
         public RhythmTimeLine TimeLineFeature = new RhythmTimeLine();
         private EditorMain _editorMain;
-
-        public RhythmEditor()
-        {
-            TimeLineFeature.CallOnUpdateTimeLineData(UpdateFilesFromTimeLine);
-        }
 
         public void DrawTimeLine(uint dockId)
         {
@@ -113,23 +171,6 @@ namespace UmbrellaToolsKit.EditorEngine.GameSettings
             DrawTimeLine(idTimeline);
             DrawProperties(idProperties);
             TimeLineFeature.TimeLineUpdate();
-        }
-
-        private void UpdateFilesFromTimeLine()
-        {
-            if (_editorMain == null) return;
-
-            foreach (var tracks in TimeLineFeature.TimeLines)
-            {
-                foreach (var item in tracks)
-                {
-                    if (item is MusicItem musicItem)
-                    {
-                        _
-                    }
-                }
-            }
-
         }
     }
 }
